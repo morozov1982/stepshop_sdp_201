9f47ea405706640353279233760b90a64a0fd27b
Александр Морозов <@new.local.itstep>
2021-08-19 12:07:49 +0600

    update: lisson_03 classwork

--- stepshop/mainapp/views.py
+++ stepshop/mainapp/views.py
@@ -2,4 +2,18 @@ from django.shortcuts import render
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    title = 'продукты | каталог'
+
+    links_menu = [
+        {'filter': '*', 'name': 'All Products'},
+        {'filter': '.new', 'name': 'Newest'},
+        {'filter': '.low', 'name': 'Low Price'},
+        {'filter': '.high', 'name': 'Hight Price'},
+    ]
+
+    context = {
+        'title': title,
+        'links_menu': links_menu,
+    }
+
+    return render(request=request, template_name='mainapp/products.html', context=context)
