0b68a795080574f1b3a938c611cdda945f7b983f
Александр Морозов <@new.local.itstep>
2021-08-20 10:41:30 +0600

    add: lesson_04_1 files

--- stepshop/mainapp/urls.py
+++ stepshop/mainapp/urls.py
@@ -1,4 +1,7 @@
+from django.conf.urls.static import static
 from django.urls import path
+
+from stepshop import settings
 from .views import products
 
 app_name = 'products'
@@ -6,3 +9,7 @@ app_name = 'products'
 urlpatterns = [
     path('', products, name='index'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
