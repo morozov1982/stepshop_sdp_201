8847bb6d3fd48d95fb78178bd66d61181610a20f
Александр Морозов <@new.local.itstep>
2021-08-20 12:01:35 +0600

    update: lesson_04 final

--- stepshop/mainapp/urls.py
+++ stepshop/mainapp/urls.py
@@ -1,7 +1,5 @@
-from django.conf.urls.static import static
 from django.urls import path
 
-from stepshop import settings
 from .views import products
 
 app_name = 'products'
@@ -9,7 +7,3 @@ app_name = 'products'
 urlpatterns = [
     path('', products, name='index'),
 ]
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- stepshop/stepshop/settings.py
+++ stepshop/stepshop/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,11 +126,11 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'stepshop', 'static'),
 )
 
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
